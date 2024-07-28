0ba7b7f0b8e9089e2110614e3706b3d4e27e187e
Hitzkii <aditya072006@gmail.com>
2024-07-28 10:57:06 +0530

    changed structure

---
+++ memalloc.h
@@ -0,0 +1,12 @@
+#include "stdio.h"
+typedef char ALIGN[16];
+
+union header {
+	struct {
+		size_t size;
+		unsigned is_free;
+		union header *next;
+	} s;
+	ALIGN stub;
+};
+typedef union header header_t;
